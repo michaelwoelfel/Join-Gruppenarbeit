79becc347bfe214704c2b1297e39bd35c066496f
Michel <woelfelm@t-online.de>
2023-07-09 10:34:39 +0200

    storage token gespeichert in index.js

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const STORAGE_TOKEN = '9DSY3OMJBPC4FF2QNS6I226NK6HRNQV27XWIWUO8';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
