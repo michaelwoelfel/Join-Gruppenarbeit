0c672f8006de78341dc2a33d977a96b272df7e7b
Matthias Berthold <matthiasberthold33@gmail.com>
2023-07-17 10:39:19 +0200

    Responsive Design Add Task - Summary

--- js/tasks.js
+++ js/tasks.js
@@ -208,37 +208,7 @@ function renderDone() {
     }
 }
 
-// PRIO COLORS CHANGING ONCLICK 
 
-function prioColorRed() {
-    let urgent = document.getElementById('prio_urgent');
-    let medium = document.getElementById('prio_medium');
-    let low = document.getElementById('prio_low');
-    urgent.classList.toggle('prio-btn-urgent-clicked');
-    medium.classList.remove('prio-btn-medium-clicked');
-    low.classList.remove('prio-btn-low-clicked');
-}
-
-
-function prioColorOrange() {
-    let urgent = document.getElementById('prio_urgent');
-    let medium = document.getElementById('prio_medium');
-    let low = document.getElementById('prio_low');
-    urgent.classList.remove('prio-btn-urgent-clicked');
-    medium.classList.toggle('prio-btn-medium-clicked');
-    low.classList.remove('prio-btn-low-clicked');
-    getTaskPrio('medium');
-}
-
-
-function prioColorGreen() {
-    let urgent = document.getElementById('prio_urgent');
-    let medium = document.getElementById('prio_medium');
-    let low = document.getElementById('prio_low');
-    urgent.classList.remove('prio-btn-urgent-clicked');
-    medium.classList.remove('prio-btn-medium-clicked');
-    low.classList.toggle('prio-btn-low-clicked');
-}
 
 
 // TEMPLATE FÜR RENDER ... WIRD FÜR JEDEN STATUS AUSGEFÜHRT 
